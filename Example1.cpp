97512f3b7d34042f2df440712f3d6627ed385929
keanguzon <123426433+keanguzon@users.noreply.github.com>
2023-08-18 12:26:41 +0800

    Add files via upload

---
+++ Example1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int length, width, ans;
+    cout << "Enter Length: ";
+    cin >> length;
+    cout << "Enter Width: ";
+    cin >> width;
+    ans = length * width;
+    cout << "Area: " << ans << endl;
+    
+    return 0;
+}
